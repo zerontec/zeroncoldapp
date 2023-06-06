1335a693dcd7612c2960516854ffd2d18bc4f339
zerontec <zerontecnologia1@gmail.com>
2023-06-06 00:32:58 -0400

    create buttonbar and modulelink component

---
+++ src/components/ButtonBar/ButtonBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, Box } from '@mui/material';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+const StyledButton = styled(Button)`
+  && {
+    background-color: #007bff;
+    color: #ffffff;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  }
+`;
+
+// eslint-disable-next-line arrow-body-style
+const ButtonBar = ({button1, button1Link, button2, button2Link, button3, button3Link }) => {
+  return (
+    <Box display="flex" justifyContent="center">
+      <ButtonContainer>
+  <Link to={button1Link} >   <StyledButton variant="contained" >
+        {  button1}
+        </StyledButton></Link>  
+        <StyledButton variant="contained" href="/enlace2">
+          {button2}
+        </StyledButton>
+        <StyledButton variant="contained" href="/enlace3">
+         {button3}
+        </StyledButton>
+        {/* <StyledButton variant="contained" href="/enlace4">
+          Reportes
+        </StyledButton> */}
+      </ButtonContainer>
+    </Box>
+  );
+};
+
+export default ButtonBar;
---
+++ src/components/ModuleLinks/ModuleLinks.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+
+const LinkBoxContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+`;
+
+const LinkBox = styled(Box)`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const LinkText = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ModuleLinks = () => {
+
+
+
+	const navigate = useNavigate();
+
+  const handleLinkClick = (link) => {
+    // Agregar lógica para redireccionar al módulo correspondiente al hacer clic en el cuadro
+	navigate(link);
+	console.log(`Redireccionando a ${link}`);
+  };
+
+  return (
+    <LinkBoxContainer>
+      <LinkBox
+        style={{ backgroundColor: '#007bff' }}
+        onClick={() => handleLinkClick('/dashboard/invoice')}
+      >
+        <LinkText>Ventas</LinkText>
+      </LinkBox>
+      <LinkBox
+        style={{ backgroundColor: '#ff6b6b' }}
+        onClick={() => handleLinkClick('/modulo2')}
+      >
+        <LinkText>Compras</LinkText>
+      </LinkBox>
+      <LinkBox
+        style={{ backgroundColor: '#00cc99' }}
+        onClick={() => handleLinkClick('/modulo3')}
+      >
+        <LinkText>Usuarios De Sistema</LinkText>
+      </LinkBox>
+      <LinkBox
+        style={{ backgroundColor: '#ffcc00' }}
+        onClick={() => handleLinkClick('/modulo4')}
+      >
+        <LinkText>Inventario</LinkText>
+      </LinkBox>
+	  <LinkBox
+      
+        style={{ backgroundColor: '#ff8c00' }}
+        onClick={() => handleLinkClick('modulo5')}
+      >
+        <LinkText>Reportes</LinkText>
+      </LinkBox>
+
+	  
+    </LinkBoxContainer>
+  );
+};
+
+export default ModuleLinks;
