66cdf47b437f066c4e41ac8038a81809a880200b
zerontec <zerontecnologia1@gmail.com>
2023-06-12 16:23:17 -0400

    Responsive component

--- src/components/ModuleLinks/ModuleLinks.jsx
+++ src/components/ModuleLinks/ModuleLinks.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Box } from '@mui/material';
 import styled from 'styled-components';
+import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
 
 const LinkBoxContainer = styled(Box)`
   display: flex;
   justify-content: center;
   gap: 16px;
+  flex-wrap: wrap; /* Agrega la propiedad flex-wrap para que los elementos se envuelvan */
 `;
 
 const LinkBox = styled(Box)`
@@ -32,15 +31,11 @@ const LinkText = styled.span`
 `;
 
 const ModuleLinks = () => {
-
-
-
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLinkClick = (link) => {
-    // Agregar lógica para redireccionar al módulo correspondiente al hacer clic en el cuadro
-	navigate(link);
-	console.log(`Redireccionando a ${link}`);
+    navigate(link);
+    console.log(`Redireccionando a ${link}`);
   };
 
   return (
@@ -63,27 +58,18 @@ const ModuleLinks = () => {
       >
         <LinkText>Usuarios De Sistema</LinkText>
       </LinkBox>
-
-
-
       <LinkBox
-        style={{ backgroundColor: '#ffcc00' }}
+        style={{ backgroundColor: '#FF5722' }}
         onClick={() => handleLinkClick('/dashboard/inventario')}
       >
         <LinkText>Inventario</LinkText>
       </LinkBox>
-
-
-
-	  <LinkBox
-      
+      <LinkBox
         style={{ backgroundColor: '#ff8c00' }}
         onClick={() => handleLinkClick('modulo5')}
       >
         <LinkText>Reportes</LinkText>
       </LinkBox>
-
-	  
     </LinkBoxContainer>
   );
 };
