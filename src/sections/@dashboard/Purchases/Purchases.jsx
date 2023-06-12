df7d7e3a33596f9eacd4781f7e2990a812bd3563
zerontec <zerontecnologia1@gmail.com>
2023-06-12 18:00:44 -0400

    no uses

--- src/sections/@dashboard/Purchases/Purchases.jsx
+++ src/sections/@dashboard/Purchases/Purchases.jsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 import Modal from '@mui/material/Modal';
 
 import { useSelector, useDispatch } from 'react-redux';
-import Snackbar from '@mui/material/Snackbar';
+
 import { fetchSupliers } from '../../../redux/modules/supplier';
 import { fetchProducts } from '../../../redux/modules/products';
 import { createPurchase, fetchPurchases } from '../../../redux/modules/purchase';
