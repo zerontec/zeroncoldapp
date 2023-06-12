6badccb0c2134ed5de77dea2f9589a54cb842c5b
zerontec <zerontecnologia1@gmail.com>
2023-06-12 16:26:03 -0400

    add title and background clor

--- src/pages/Invoices.js
+++ src/pages/Invoices.js
@@ -1,21 +1,34 @@
 
 import React, { useState } from 'react';
 import {  Typography,  } from '@mui/material';
+import styled from 'styled-components';
+import Box from "@mui/material/Box";
 import InvoiceTable from '../sections/@dashboard/Invoicetable/Invoicetable';
 import { ButtonBar } from '../components/ButtonBar';
 
-
-
+const FormTipo = styled.div`
+  display: flex;
+  gap: 3rem;
+  width: 100%;
+  max-width: 1000px;
+  margin-bottom: 2rem;
+  background-color: rgb(0, 123, 255);
+  border-radius: 20px;
+`
 
 
 const InvoicePages = () => (
     <>
 
-<Typography variant="h4" sx={{ mb: 5 }}>
-         Lista de Facturas 
-        </Typography>
 
-<ButtonBar button1={"Cuentas Por Cobrar"}
+<FormTipo>
+ <Typography style={{marginLeft:15, marginTop:10}} color="white" variant="h5" sx={{ marginBottom: 2 }}>
+         Lista de facturas
+        </Typography>
+        </FormTipo>
+<ButtonBar 
+button1={"Cuentas Por Cobrar"}
+button1Link={'/dashboard/cuentasxc'}
 button2={"Clientes"}
 button3={"Reporte Ventas"}
            
--- src/pages/PurchasePages.js
+++ src/pages/PurchasePages.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
+import {Typography } from '@mui/material';
 import {Purchases } from '../sections/@dashboard/Purchases'
 
+
+const FormTipo = styled.div`
+  display: flex;
+  gap: 3rem;
+  width: 100%;
+  max-width: 1000px;
+  margin-bottom: 2rem;
+  background-color: rgb(255, 107, 107);
+  border-radius: 20px;
+`
 
 
 const PurchasePage = () => (
     <>
+    <FormTipo>
+ <Typography style={{marginLeft:15, marginTop:10}} color="white" variant="h5" sx={{ marginBottom: 2 }}>
+          Resumen de Compra
+        </Typography>
+        </FormTipo>
+
   <Purchases/>
    
     </>
