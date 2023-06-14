970c580a20a381a78d3ffba00ad336869735a869
zerontec <zerontecnologia1@gmail.com>
2023-06-14 00:49:58 -0400

    user symtem page

---
+++ src/pages/UserSystemPages.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { UserSystem } from '../sections/@dashboard/UserSystem'
+
+
+
+
+
+
+
+const UserSystemPage =() => (
+
+<>
+
+<UserSystem/>
+</>
+
+
+)
+
+
+export default UserSystemPage
