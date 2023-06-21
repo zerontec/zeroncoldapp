414893838ef3b057722c4a0982aecf48e8b4fdca
zerontec <zerontecnologia1@gmail.com>
2023-06-20 23:34:50 -0400

    delete no uses

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,14 +3,14 @@ import { Helmet } from 'react-helmet-async';
 
 // @mui
 import { styled } from '@mui/material/styles';
-import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
+import {  Container, Typography, Divider} from '@mui/material';
 
 
 // hooks
 import useResponsive from '../hooks/useResponsive';
 // components
 import Logo from '../components/logo';
-import Iconify from '../components/iconify';
+
 // sections
 import { LoginForm } from '../sections/auth/login';
 
