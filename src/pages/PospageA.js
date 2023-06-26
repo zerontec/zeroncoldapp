9f956fcc8ed3d000085f6dc8a45d127915a44a45
zerontec <zerontecnologia1@gmail.com>
2023-06-26 13:32:37 -0400

    postPagaA

---
+++ src/pages/PospageA.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { PosA } from '../sections/@dashboard/PosA'
+
+
+const PosPageA = () => (
+    <>
+    <PosA/>
+   
+    </>
+  );
+
+export default PosPageA;
+
