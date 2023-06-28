14fa4a4fa556a3fa468747b3be188cc728cf0114
zerontec <zerontecnologia1@gmail.com>
2023-06-28 11:07:26 -0400

    change style

--- src/pages/CuentasxcPages.js
+++ src/pages/CuentasxcPages.js
@@ -1,18 +1,30 @@
 
 import React, { useState } from 'react';
+
+import styled from 'styled-components';
+
 import { Grid, Container, Typography,  } from '@mui/material';
 import { CuentasPorCobrar } from '../sections/@dashboard/CuentasPorCobrar';
 
-
-
+const FormTipo = styled.div`
+  display: flex;
+  gap: 3rem;
+  width: 100%;
+  max-width: 1000px;
+  margin-bottom: 2rem;
+  background-color: rgb(0, 123, 255);
+  border-radius: 20px;
+`
 
 
 const CuentasxPages = () => (
     <>
 
-<Typography variant="h4" sx={{ mb: 5 }}>
-       Cuentas Por cobrar 
+<FormTipo>
+ <Typography style={{marginLeft:15, marginTop:10}} color="white" variant="h5" sx={{ marginBottom: 2 }}>
+         Cuentas Por Cobrar
         </Typography>
+        </FormTipo>
 
 
 <CuentasPorCobrar/>
