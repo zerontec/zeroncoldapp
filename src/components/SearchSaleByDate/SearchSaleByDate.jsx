ca76eb1ce6b68cc4eb68b71a93c3dc6590457397
zerontec <zerontecnologia1@gmail.com>
2023-06-30 01:38:32 -0400

    change url api

--- src/components/SearchSaleByDate/SearchSaleByDate.jsx
+++ src/components/SearchSaleByDate/SearchSaleByDate.jsx
@@ -84,7 +84,7 @@ const SearchSaleByDate = () => {
 		  console.log(startDate,endDate);
 		  
 		  try {
-			const response = await axios.get(`${API_URL_D}api/report/daily-report/${startDate}/${endDate}`);
+			const response = await axios.get(`${API_URL}api/report/daily-report/${startDate}/${endDate}`);
 	
 			if (response) {
 			  setSuccessMessage("Venta para la fecha");
