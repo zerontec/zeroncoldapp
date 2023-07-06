f6d465ffafb8b26bafa2b0465e78523b36bdecb4
zerontec <zerontecnologia1@gmail.com>
2023-07-06 00:13:42 -0400

    fixed button delete!

--- src/sections/@dashboard/NotasCredito/NotasCredito.jsx
+++ src/sections/@dashboard/NotasCredito/NotasCredito.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState, useEffect } from 'react';
 
 import TableContainer from '@mui/material/TableContainer';
