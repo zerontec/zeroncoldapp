ba9cfc4c4f8ab562eaf47618d676f65b204ea9e5
zerontec <zerontecnologia1@gmail.com>
2023-07-06 22:05:11 -0400

    add edit prestamo

--- src/sections/@dashboard/AdminPerfil/AdminPerfil.jsx
+++ src/sections/@dashboard/AdminPerfil/AdminPerfil.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
+
 import { useParams, Link } from 'react-router-dom';
-import Paper from "@mui/material/Paper";
-import TablePagination from "@mui/material/TablePagination";
+
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import Modal from '@mui/material/Modal';
 import {
   Box,
   Container,
@@ -13,230 +20,378 @@ import {
   TableHead,
   TableRow,
   Typography,
-
+  TextField,
+  Button,
 } from '@mui/material';
-
-import { useSelector, useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
 import { DriveFolderUpload } from '@mui/icons-material';
-// import { getCustomerById } from   'tu_accion_para_obtener_cliente_por_id';
-// import { getCustomerPurchases } from 'tu_accion_para_obtener_compras_de_cliente';
 import { serachUsereById } from '../../../redux/modules/user';
-import { getAllLoans } from '../../../redux/modules/loan';
+import { deleteUpload, getAllLoans, updateLoand } from '../../../redux/modules/loan';
 
 import { fDate, fDateTime } from '../../../utils/formatTime';
 import { FloatingButtonComponent } from '../../../components/FloatingButtonComponent';
 import { CreateLoan } from '../../../components/CreateLoan';
 
-
 const columns = [
-	{
-	  id: "id",
-	  label: "Id",
-	  minWidth: 50,
-	},
-	{
-		id: "name",
-		label: "Nombre",
-		minWidth: 100,
-	  },
-	{
-	  id: "name",
-	  label: "Cantidad",
-	  minWidth: 100,
-	},
-	{
-	  id: "age",
-	  label: "Status",
-	  minWidth: 50,
-	},
-	{
-		id: "fecha",
-		label: "Fecha ",
-		minWidth: 50,
-	  },
-	  {
-		id: "notes",
-		label: "Motivo",
-		minWidth: 50,
-	  },
-	  
-	  
-	
-  ];
-
+  {
+    id: 'id',
+    label: 'Id',
+    minWidth: 50,
+  },
+  {
+    id: 'name',
+    label: 'Nombre',
+    minWidth: 100,
+  },
+  {
+    id: 'name',
+    label: 'Cantidad',
+    minWidth: 100,
+  },
+  {
+    id: 'age',
+    label: 'Status',
+    minWidth: 50,
+  },
+  {
+    id: 'fecha',
+    label: 'Fecha ',
+    minWidth: 50,
+  },
+  {
+    id: 'notes',
+    label: 'Motivo',
+    minWidth: 50,
+  },
+];
 
 const StyledContainer = styled(Container)`
   padding-top: 24px;
   padding-bottom: 24px;
 `;
+const ActionsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+`;
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+`;
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 16px;
+
+  & > * {
+    margin-bottom: 8px;
+  }
+`;
 
 const AdminPerfil = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedLoanId, setSelectedLoanId] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const [deudas, setDeudas] = useState([]);
 
-	const { id} = useParams();
-	const dispatch = useDispatch();
+  const handleEditClick = (loan) => {
+    setSelectedLoanId(loan.id);
+    setSelectedLoanEdit({
+      amount: loan.amount,
+      notes: loan.notes,
+      status: loan.status,
+    });
+    setOpen(true);
+  };
 
-	const [page, setPage] = useState(0);
-	const [rowsPerPage, setRowsPerPage] = useState(5);
-	const [searchTerm, setSearchTerm] = useState("");
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-  
-	const [user, setUser] = useState(null);
+  const handleCloseModal = () => {
+    setSelectedLoanId(null);
+    setSelectedLoanEdit({
+      amount: '',
+      notes: '',
+      status: '',
+    });
+    setOpen(false);
+  };
 
-	const [deudas, setDeudas] = useState([]);
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage);
-	  };
-	
-	  const handleChangeRowsPerPage = (event) => {
-		setRowsPerPage(parseInt(event.target.value, 10));
-		setPage(0);
-	  };
-	
-	
+  const [selectedLoanEdit, setSelectedLoanEdit] = useState({
+    amount: '',
+    nota: '',
+    status: '',
+  });
+  const handleSubmitEdit = (e) => {
+    if (selectedLoanEdit.amount && selectedLoanEdit.notes) {
+      e.preventDefault();
 
-const usuarios = useSelector((state)=> state.usuarios)
-const deuda = useSelector((state) => state.loan)
-console.log("usuarios",usuarios)
-console.log("deuda", deuda)
-useEffect(() => {
+      const data = {
+        ...selectedLoanEdit,
+        id: selectedLoanId,
+      };
+      dispatch(updateLoand(selectedLoanId, data));
+      Swal.fire('¨Prestamo Editado con Exito  !', 'You clicked the button!', 'success');
+      dispatch(getAllLoans());
 
-	  // Llamada a la API para obtener los datos del cliente
-	  dispatch(serachUsereById({id}))
-		.then((response) => setUser(response))
-		.catch((error) => console.log(error));
-		// dispatch(getClientPurchase({id}))
-		// .then((response) => setCompras(response))
-		// .catch((error) => console.log(error));
-  
-		dispatch(getAllLoans())
-		.then((response) => setDeudas(response))
-		.catch((error) => console.log(error));
+      handleCloseModal();
+      //   getAllAnalysis();
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debe completar toda la informacion !',
+      });
 
-	}, [id, dispatch]);
-  
+      handleCloseModal();
+    }
+  };
 
-fDateTime()
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-	return (
-	  <StyledContainer>
-		{/* {customer && ( */}
-		  <>
-			<Box sx={{ marginBottom: '24px' }}>
-			  <Typography variant="h4" component="h1">
-				Perfil de Administrador:
-			  </Typography>
-			  <Typography variant="body1">
-				
-				<strong>Nombre: {usuarios.usuarios.name}</strong> 
-			  </Typography>
-			  <Typography variant="body1">
-				
-				<strong>Cédula o Rif: {usuarios.usuarios.username}</strong> 
-			  </Typography>
-			 
-			  <Typography variant="body1">
-				
-				<strong>Direccion: {usuarios.usuarios.email}</strong> 
-			  </Typography> 
+  const usuarios = useSelector((state) => state.usuarios);
+  const deuda = useSelector((state) => state.loan);
+  console.log('usuarios', usuarios);
+  console.log('deuda', deuda);
+  useEffect(() => {
+    // Llamada a la API para obtener los datos del cliente
+    dispatch(serachUsereById({ id }))
+      .then((response) => setUser(response))
+      .catch((error) => console.log(error));
+    // dispatch(getClientPurchase({id}))
+    // .then((response) => setCompras(response))
+    // .catch((error) => console.log(error));
 
-			   <Typography variant="body1">
-				<strong>Fecha de Registro: {fDateTime(usuarios.usuarios.createdAt)}</strong> 
-			  </Typography> 
-			   {/* <Typography variant="body1">
-				
-				<strong>Total gastado: {compra.invoices. totalPurchases}</strong> 
-			  </Typography> */}
-			  {/* Agrega más datos del cliente según tus necesidades */}
-			
-			<CreateLoan/>
-			
-			
-			</Box>
+    dispatch(getAllLoans())
+      .then((response) => setDeudas(response))
+      .catch((error) => console.log(error));
+  }, [id, dispatch]);
 
+  function deleteHandler(item) {
+    Swal.fire({
+      title: 'Estas Seguro',
+      text: 'No podras revertir esta operacion !',
+      icon: 'advertencia',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Borrar!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteUpload(item.id));
+        Swal.fire('la deuda  ha sido borrado!');
 
+        setTimeout(() => {
+          window.location.reload();
+        }, 500);
+      } else {
+        Swal.fire('la cuenta  Esta Seguro !');
+      }
+    });
+  }
 
-			<Typography variant="h5" component="h3">
-				Deudas Empleados 
-			  </Typography>
+  fDateTime();
 
-		  
-			  <TableContainer component={Paper}>
-  <Table sx={{ minWidth: 650 }}>
-    <TableHead>
-      <TableRow>
-        {columns.map((column) => (
-          <TableCell
-            key={column.id}
-            align="left"
-            minWidth={column.minWidth}
-          >
-            {column.label}
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-	<TableBody>
-  {Array.isArray(deuda.loans) && deuda.loans.length > 0 ? (
-    deuda.loans
-      .filter((item) =>
-        item.amount.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-      .map((item) => (
-        <TableRow key={item.id}>
-          <TableCell align="left">{item.seller?.codigo}</TableCell>
-		  <TableCell align="left">{item.seller?.name}</TableCell>
-          <TableCell align="left">{item.amount}</TableCell>
-          <TableCell align="left">{item.status}</TableCell>
-          <TableCell align="left">{fDateTime(item.createdAt)}</TableCell>
-          <TableCell align="left">{item.notes}</TableCell>
-          {/* Agrega más columnas según las propiedades de la deuda */}
-        
-		  <Link
-              to={`/dashboard/perfil-empleados/${item.seller?.id}`}
-              style={{ textDecoration: 'none' }}
+  return (
+    <>
+      <Modal open={open} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            borderRadius: '8px',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <h2>Editar Prestamo</h2>
+          {selectedLoanEdit && (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
             >
-              <button>Ver perfil</button>
-            </Link>
-		</TableRow>
-      ))
-  ) : (
-	
-    <TableRow>
-		   
-      <TableCell colSpan={columns.length}>No hay datos disponibles</TableCell>
-    </TableRow>
-  )}
-</TableBody>
+              <FormContainer>
+                <FieldContainer>
+                  <TextField
+                    type="text"
+                    value={setSelectedLoanId.id}
+                    onChange={(e) =>
+                      setSelectedLoanId({
+                        ...selectedLoanId,
+                        id: e.target.value,
+                      })
+                    }
+                  />
 
+                  {/* <TextField
+                    label="Cantidad"
+                    type="number"
+                    value={selectedLoanEdit.amount}
+                    onChange={(e) =>
+                      setSelectedLoanEdit({
+                        ...selectedLoanEdit,
+                        amount: e.target.value,
+                      })
+                    }
+                  /> */}
 
+                  <br />
+                  <TextField
+                    label="Nota"
+                    name="notes"
+                    value={selectedLoanEdit.notes}
+                    onChange={(e) =>
+                      setSelectedLoanEdit({
+                        ...selectedLoanEdit,
+                        notes: e.target.value,
+                      })
+                    }
+                  />
+                  <TextField
+                    label="Estatus"
+                    name="status"
+                    value={selectedLoanEdit.status}
+                    onChange={(e) =>
+                      setSelectedLoanEdit({
+                        ...selectedLoanEdit,
+                        status: e.target.value,
+                      })
+                    }
+                  />
 
+                  <br />
+                </FieldContainer>
+                <ActionsContainer>
+                  <Button variant="contained" type="submit" color="primary" onClick={handleSubmitEdit}>
+                    Guardar cambios
+                  </Button>
+                </ActionsContainer>
+              </FormContainer>
+            </form>
+          )}
+          <hr />
+          <Button variant="contained" onClick={() => setOpen(null)}>
+            Cerrar
+          </Button>
+        </Box>
+      </Modal>
+      <StyledContainer>
+        {/* {customer && ( */}
+        <>
+          <Box sx={{ marginBottom: '24px' }}>
+            <Typography variant="h4" component="h1">
+              Perfil de Administrador:
+            </Typography>
+            <Typography variant="body1">
+              <strong>Nombre: {usuarios.usuarios.name}</strong>
+            </Typography>
+            <Typography variant="body1">
+              <strong>Cédula o Rif: {usuarios.usuarios.username}</strong>
+            </Typography>
 
+            <Typography variant="body1">
+              <strong>Direccion: {usuarios.usuarios.email}</strong>
+            </Typography>
 
+            <Typography variant="body1">
+              <strong>Fecha de Registro: {fDateTime(usuarios.usuarios.createdAt)}</strong>
+            </Typography>
+            {/* <Typography variant="body1">
+     
+     <strong>Total gastado: {compra.invoices. totalPurchases}</strong>
+   </Typography> */}
+            {/* Agrega más datos del cliente según tus necesidades */}
 
+            <CreateLoan />
+          </Box>
 
-  </Table>
-  <TablePagination
-    rowsPerPageOptions={[5, 10, 100]}
-    component="div"
-    count={deuda.loans.length}
-    rowsPerPage={rowsPerPage}
-    page={page}
-    onPageChange={handleChangePage}
-    onRowsPerPageChange={handleChangeRowsPerPage}
-  />
-</TableContainer>
+          <Typography variant="h5" component="h3">
+            Deudas Empleados
+          </Typography>
 
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell key={column.id} align="left" minWidth={column.minWidth}>
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Array.isArray(deuda.loans) && deuda.loans.length > 0 ? (
+                  deuda.loans
+                    .filter((item) => item.amount.toLowerCase().includes(searchTerm.toLowerCase()))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((item) => (
+                      <TableRow key={item.id}>
+                        <TableCell align="left">{item.seller?.codigo}</TableCell>
+                        <TableCell align="left">{item.seller?.name}</TableCell>
+                        <TableCell align="left">{item.amount}</TableCell>
+                        <TableCell align="left">{item.status}</TableCell>
+                        <TableCell align="left">{fDateTime(item.createdAt)}</TableCell>
+                        <TableCell align="left">{item.notes}</TableCell>
+                        {/* Agrega más columnas según las propiedades de la deuda */}
+                        <TableCell className="tableCell">
+                          <Button variant="contained" onClick={() => handleEditClick(item)}>
+                            Editar
+                          </Button>
+                        </TableCell>
+                        <TableCell className="tableCell">
+                          <div className="deleteButton" id={item.id} onClick={() => deleteHandler(item)}>
+                            <Button>Borrar</Button>
+                          </div>
+                        </TableCell>
+                        <Link to={`/dashboard/perfil-empleados/${item.seller?.id}`} style={{ textDecoration: 'none' }}>
+                          <button>Ver perfil</button>
+                        </Link>
+                      </TableRow>
+                    ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={columns.length}>No hay datos disponibles</TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 100]}
+              component="div"
+              count={deuda.loans.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableContainer>
 
-<FloatingButtonComponent/>
-		  </>
-		{/* // )} */}
-	  </StyledContainer>
-	);
-
-
-	
+          <FloatingButtonComponent />
+        </>
+        {/* // )} */}
+      </StyledContainer>
+    </>
+  );
 };
-
 
 export default AdminPerfil;
--- src/sections/@dashboard/SellerPerfil/SellerPerfil.jsx
+++ src/sections/@dashboard/SellerPerfil/SellerPerfil.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Paper from "@mui/material/Paper";
 import TablePagination from "@mui/material/TablePagination";
 import styled from 'styled-components';
+import Swal from 'sweetalert2';
 import {
   Box,
   Container,
@@ -19,7 +20,7 @@ import {
 
 } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
-import { getSalesStats, serachSellerById } from '../../../redux/modules/seller';
+import { getSalesStats, getSellerDebt, serachSellerById } from '../../../redux/modules/seller';
 
 
 import { fDate, fDateTime } from '../../../utils/formatTime';
@@ -29,17 +30,17 @@ import { FloatingButtonComponent } from '../../../components/FloatingButtonCompo
 const columns = [
 	{
 	  id: "id",
-	  label: "codigo",
+	  label: "Numero factura ",
 	  minWidth: 50,
 	},
 	{
 	  id: "name",
-	  label: "Producto",
+	  label: "Codigo Producto",
 	  minWidth: 100,
 	},
 	{
 	  id: "age",
-	  label: "Monto",
+	  label: "Producto",
 	  minWidth: 50,
 	},
 	{
@@ -47,6 +48,11 @@ const columns = [
 		label: "cantidad",
 		minWidth: 50,
 	  },
+    {
+      id: "quantity",
+      label: "Total Vendido",
+      minWidth: 50,
+      },
   ];
 
 
@@ -68,6 +74,10 @@ const SellerPerfil = () => {
 	const [seller, setSeller] = useState(null);
 	const [sales, setSales] = useState([]);
 	const [products, setProduct] = useState([]);
+
+	const [debts, setDebts] = useState(null);
+
+  
 	
 	const handleChangePage = (event, newPage) => {
 		setPage(newPage);
@@ -80,12 +90,33 @@ const SellerPerfil = () => {
 	
 	
 	
-
+    // function deleteHandler(item) {
+    //   Swal.fire({
+    //     title: 'Estas Seguro',
+    //     text: 'No podras revertir esta operacion !',
+    //     icon: 'advertencia',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Si, Borrar!',
+    //   }).then((result) => {
+    //     if (result.isConfirmed) {
+    //       dispatch(deleteUpload(item.id));
+    //       Swal.fire('la deuda  ha sido borrado!');
+  
+    //       setTimeout(() => {
+    //         window.location.reload();
+    //       }, 500);
+    //     } else {
+    //       Swal.fire('la cuenta  Esta Seguro !');
+    //     }
+    //   });
+    // }
 const empleado = useSelector((state)=> state.vendedores)
 const productos = useSelector((state)=> state.invoice)
 console.log(empleado)
 console.log(productos)
-
+console.log("debts", debts)
 useEffect(() => {
 
 	  // Llamada a la API para obtener los datos del cliente
@@ -98,7 +129,9 @@ useEffect(() => {
 	dispatch(getProductsBySeller({id}))
 	.then((response) => setProduct(response))
 		.catch((error) => console.log(error));	
-
+    dispatch(getSellerDebt({id}))
+    .then((response) => setDebts(response))
+      .catch((error) => console.log(error));
 
 	}, [id, dispatch]);
   
@@ -130,8 +163,17 @@ fDateTime()
 			  <Typography variant="body1">
 				<strong>Fecha de Registro: {fDateTime(empleado.vendedores.createdAt)}</strong> 
 			  </Typography>
+
+          <Typography variant="body1">
+				<strong>Fecha de Registro: </strong> 
+			  </Typography>
+        <hr/>
+        <Typography variant="subtitle1">Deudas por Prestamos o Adelantos:</Typography>
+          <Typography variant="h4">${debts?.debt}</Typography>
 			   {/* <Typography variant="body1">
 				
+
+        {info.debts}
 				<strong>Total gastado: {compra.invoices. totalPurchases}</strong> 
 			  </Typography> */}
 			  {/* Agrega más datos del cliente según tus necesidades */}
@@ -146,19 +188,22 @@ fDateTime()
           <Typography variant="subtitle1">Ventas del día:</Typography>
           <Typography variant="h4">Bs.{sales.daySales}</Typography>
         </Grid>
-        <Grid item xs={4}>
+        {/* <Grid item xs={4}>
           <Typography variant="subtitle1">Ventas de la semana:</Typography>
           <Typography variant="h4">Bs.{sales.weekSales}</Typography>
         </Grid>
         <Grid item xs={4}>
           <Typography variant="subtitle1">Ventas del mes:</Typography>
           <Typography variant="h4">Bs.{sales.monthSales}</Typography>
-        </Grid>
+        </Grid> */}
       </Grid>
     </Paper>
 
 
 	<TableContainer component={Paper}>
+  <Typography variant="h6">Productos Vendidos </Typography>
+
+
           <Table sx={{ minWidth: 650 }}>
             <TableHead>
               <TableRow>
@@ -183,13 +228,19 @@ fDateTime()
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((items) => (
                   <TableRow key={items.id}>
+                     <TableCell align="left"> {items.invoiceNumber}</TableCell>
                     <TableCell align="left"> {items.barcode}</TableCell>
                     <TableCell align="left"> {items.name}</TableCell>
-                   
-                    <TableCell align="left"> {items.price}</TableCell>
-                    
                     <TableCell align="left"> {items.quantity}</TableCell>
-					<>
+                    <TableCell align="left"> {items.price}</TableCell>
+                    {/* <TableCell className="tableCell">
+                          <div className="deleteButton" id={item.id} onClick={() => deleteHandler(item)}>
+                            <Button>Borrar</Button>
+                          </div>
+                        </TableCell>
+                    */}
+					<> 
+                   
                       {/* <TableCell className="tableCell">
                         <Button
                           variant="contained"
