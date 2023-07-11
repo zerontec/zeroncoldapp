8128f57aabbd2d1291535fd430ab659c95372885
zerontec <zerontecnologia1@gmail.com>
2023-07-11 16:58:44 -0400

    addloand module

--- src/redux/modules/loan.js
+++ src/redux/modules/loan.js
@@ -16,6 +16,9 @@ const CREATE_LOAN_ERROR = 'CREATE_LOAN_ERROR';
 const SEARCH_DATE_SUCCESS = "SEARCH_DATE_SUCCESS";
 const UPDATE_LOAND = "UPDATE_LOAN"
 const DELETE_LOAN ="DELETE_LOAN"
+const CREATE_PAYMENT_SUCCESS ='CREATE_PAYMENT_SUCCESS'
+const CREATE_PAYMENT='CREATE_PAYMENT'
+const UPDATE_PAYMENT= 'UPDATE_PAYMENT'
 // const SEARCH_DATE_REQUEST = "SEARCH_DATE_REQUEST";
 // const SEARCH_DATE_ERROR = "SEARCH_DATE_ERROR";
 export const fetchLoanRequest = () => ({
@@ -36,7 +39,7 @@ export const fetchLoanFailure = (error) => ({
 export const fetchLoans = (query) => async (dispatch) => {
   dispatch(fetchLoanRequest());
   try {
-    const response = await fetch(`https://expressjs-postgres-production-bd69.up.railway.app/api/loan/search-query?q=${query}`);
+    const response = await fetch(`http://localhost:5040/api/loan/search-query?q=${query}`);
     const data = await response.json();
     dispatch(fetchLoanSuccess(data));
     return data;
@@ -48,7 +51,7 @@ export const fetchLoans = (query) => async (dispatch) => {
 
   export const getAllLoans = () => async (dispatch) => {
     try {
-      const resp = await axios.get(`${API_URL}api/loan/get-all`);
+      const resp = await axios.get(`${API_URL}api/loan/get-all`,{ headers: authHeader() });
   
       dispatch({ type: GET_LOANS, payload: resp.data });
   
@@ -65,7 +68,7 @@ export const fetchLoans = (query) => async (dispatch) => {
 
 export const createLoans = (invoiceData) => async (dispatch) => {
   try {
-    const { data } = await axios.post(`${API_URL}api/loan/create`, invoiceData);
+    const { data } = await axios.post(`${API_URL}api/loan/create`, invoiceData,{ headers: authHeader() });
     dispatch({
       type: CREATE_LOAN_SUCCESS,
       payload: data,
@@ -82,6 +85,29 @@ export const createLoans = (invoiceData) => async (dispatch) => {
   
   }
 };
+
+
+export const createPayment = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${API_URL}api/loan/payment`, data,{ headers: authHeader() });
+    dispatch({
+      type: CREATE_PAYMENT,
+      payload: response.data,
+    });
+    return response.data;
+    // Aquí podrías enviar una notificación de éxito al usuario
+  } catch (error) {
+    if (error.response && error.response.status ) {
+      throw new Error("No se puedo crear el abono");
+  
+    }
+    console.error(error)
+    throw error;
+    // Aquí podrías enviar una notificación de error al usuario
+  
+  }
+};
+
 
 
 
@@ -104,9 +130,11 @@ export const searchLoanByDate = (startDate, endDate) => async (dispatch) => {
   }
 };
 
+
+
 export const updateLoand = (id, data) => async (dispatch) => {
     try {
-      const resp = await axios.put(`${API_URL}api/loan/update/${id}`, data);
+      const resp = await axios.put(`${API_URL_D}api/loan/update/${id}`, data);
   
       dispatch({
         type: UPDATE_LOAND,
@@ -120,10 +148,30 @@ export const updateLoand = (id, data) => async (dispatch) => {
   };
 
 
+  export const updatePayment = (id, data) => async (dispatch) => {
+    try {
+      const resp = await axios.put(`${API_URL_D}api/loan/update-payment/${id}`, data);
+  
+      dispatch({
+        type: UPDATE_PAYMENT,
+        payload: resp.data,
+      });
+  
+      return resp.data;
+    } catch (err) {
+      return err.response;
+    }
+  };
+
+
+
+
+
+
   export const deleteUpload =(id) => async (dispatch)=> {
 
 try{
-    const resp = await axios.delete(`${API_URL}api/loan/delete/${id}`)
+    const resp = await axios.delete(`${API_URL_D}api/loan/delete/${id}`)
     dispatch({
     type:DELETE_LOAN,
     payload:resp.data
@@ -146,7 +194,25 @@ return resp.data;
 export const getLoanBySeller=({id}) => async (dispatch) => {
 
   try {
-    const response = await axios.get(`${API_URL}api/loan/seller-product-sales/${id}`,{ headers: authHeader() }
+    const response = await axios.get(`${API_URL_D}api/loan/seller-product-sales/${id}`,{ headers: authHeader() }
+    );
+  
+    dispatch({
+      type: SEARCH_DATE_SUCCESS,
+      payload: response.data,
+    });
+    return response.data;
+  } catch (error) {
+    dispatch(fetchLoanFailure(error.message));
+    return null
+  }
+};
+
+
+export const getAllPayment=() => async (dispatch) => {
+
+  try {
+    const response = await axios.get(`${API_URL_D}api/loan/get-all-payment`,{ headers: authHeader() }
     );
   
     dispatch({
@@ -177,11 +243,18 @@ export default function loanReducer(state = initialState, action) {
         loans: action.payload,
       };
 
-    case CREATE_LOAN:
+    case CREATE_PAYMENT:
+      return {
+        ...state,
+        loans: action.payload,
+      };
+      
+    case CREATE_PAYMENT_SUCCESS:
       return {
         ...state,
         sendLoan: action.payload,
       };
+
 
     case CREATE_LOAN_ERROR:
       return {
@@ -206,7 +279,13 @@ export default function loanReducer(state = initialState, action) {
     
     
             }
-
+            case UPDATE_PAYMENT:
+              return{
+              ...state,
+              loans:action.payload
+      
+      
+              }
 
             case DELETE_LOAN:
                 return{
