9e1534a2772532722f91f8bc8993d0c24d106aee
zerontec <zerontecnologia1@gmail.com>
2023-07-11 16:57:02 -0400

    add payment table and module payment

---
+++ src/components/PaymentTable/PaymentTable.jsx
@@ -0,0 +1,204 @@
+import React, {useState, useEffect} from 'react';
+
+import styled from 'styled-components';
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+	Box,
+	Button,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TextField,
+	Typography
+  
+  } from '@mui/material';
+  import { fDate, fDateTime } from '../../utils/formatTime';
+
+  import { getAllPayment } from '../../redux/modules/payments';
+
+
+
+  const columns = [
+	{
+		id: "id",
+		label: "Id de Deuda",
+		minWidth: 50,
+	  },
+	{
+	  id: "id",
+	  label: "Monto Abonado",
+	  minWidth: 50,
+	},
+	{
+	  id: "name",
+	  label: "Fecha de Abono",
+	  minWidth: 100,
+	},
+	{
+	  id: "age",
+	  label: "Empleado",
+	  minWidth: 50,
+	},
+	{
+	  id: "gender",
+	  label: "Codigo Empleado",
+	  minWidth: 50,
+	},
+	// {
+	//   id: "address",
+	//   label: "Afectada por Devolucion",
+	//   minWidth: 150,
+	// },
+  ];
+
+const PaymentTable = () => {
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(5);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [selectedAbono, setSelectedAbono] = useState(null);
+	const dispatch = useDispatch();
+	const [abono, setAbono]=useState([])
+
+
+fDateTime();
+
+
+const abonos = useSelector((state)=> state.payment)
+console.log("abonos", abonos)
+
+
+
+useEffect(() => {
+	dispatch(getAllPayment())
+	  .then((response) => {
+		if (Array.isArray(response)) {
+		  setAbono(response);
+		} else {
+		  setAbono([]);
+		}
+	  })
+	  .catch((error) => console.log(error));
+  }, []);
+
+
+const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+	return(
+		<>
+		
+<Box sx={{ m: 2 }}>
+
+  
+<Typography variant="h5" component="h3">
+            Abonos Realizados
+          </Typography>
+		  
+  
+<TextField
+          label="Buscar Abonos"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+       
+  
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                {" "}
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align="left"
+                    minWidth={column.minWidth}
+                  >
+                    {" "}
+                    {column.label}{" "}
+                  </TableCell>
+                ))}{" "}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {" "}
+              {Array.isArray(abonos.payments) && abonos.payments.length > 0 ? (
+        abonos.payments
+          .filter((item) =>
+            item.seller?.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          .map((item) => (
+            
+              <TableRow key={item.id} >
+				<TableCell align="left">{item.loanId}</TableCell>
+                <TableCell align="left">{item.amount}</TableCell>
+                 <TableCell align="left">{fDateTime(item.createdAt)}</TableCell> 
+                <TableCell align="left">{item.seller?.name}</TableCell>
+				<TableCell align="left">{item.seller?.codigo}</TableCell>
+               
+               
+                {/* ... */}
+                <>
+                      {/* <TableCell className="tableCell">
+                        <Button
+                          variant="contained"
+                          onClick={() => setSelectedAbono(item)}
+                        >
+                          Ver
+                        </Button>
+                      </TableCell> */}
+                      
+                    </>
+              </TableRow>
+
+              
+            )
+          )
+      ) : (
+        <TableRow>
+          <TableCell colSpan={6}>No hay datos disponibles</TableCell>
+        </TableRow>
+      )}
+  </TableBody>
+  
+</Table>
+          <TablePagination
+            rowsPerPageOptions={[5,10, 100]}
+            component="div"
+			count={Array.isArray(abonos.payments) ? abonos.payments.length : 0}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+           />
+        </TableContainer>
+      </Box>
+		
+		
+		
+		</>
+
+
+	);
+};
+
+export const PaymentTableStl = styled.div``;
+
+PaymentTable.propTypes = {};
+
+export default PaymentTable;
---
+++ src/redux/modules/payments.js
@@ -0,0 +1,197 @@
+
+
+import axios from 'axios';
+import authHeader from '../services/auth-header';
+
+
+const API_URL_D = "http://localhost:5040/";
+const API_URL = "https://expressjs-postgres-production-bd69.up.railway.app/"
+const FETCH_LOAN_REQUEST = 'FETCH_LOAN_REQUEST';
+const FETCH_LOAN_SUCCESS = 'FETCH_LOAN_SUCCESS';
+const FETCH_LOAN_FAILURE = 'FETCH_LOAN_FAILURE';
+
+const GET_LOANS = 'GET_LOANS';
+const CREATE_LOAN_SUCCESS = 'CREATE_LOAN_SUCCESS';
+const CREATE_LOAN_ERROR = 'CREATE_LOAN_ERROR';
+const SEARCH_DATE_SUCCESS = "SEARCH_DATE_SUCCESS";
+
+const CREATE_PAYMENT_SUCCESS ='CREATE_PAYMENT_SUCCESS'
+const CREATE_PAYMENT='CREATE_PAYMENT'
+const UPDATE_PAYMENT= 'UPDATE_PAYMENT'
+const GET_ALL_PAYMENT='GET_ALL_PAYMENT'
+
+
+
+export const fetchLoanRequest = () => ({
+  type: FETCH_LOAN_REQUEST,
+});
+
+export const fetchLoanSuccess = (customers) => ({
+  type: FETCH_LOAN_SUCCESS,
+  payload: customers,
+});
+
+export const fetchLoanFailure = (error) => ({
+  type: FETCH_LOAN_FAILURE,
+  payload: error,
+});
+
+
+export const fetchLoans = (query) => async (dispatch) => {
+  dispatch(fetchLoanRequest());
+  try {
+    const response = await fetch(`http://localhost:5040/api/loan/search-query?q=${query}`);
+    const data = await response.json();
+    dispatch(fetchLoanSuccess(data));
+    return data;
+  } catch (error) {
+    dispatch(fetchLoanRequest(error.message));
+    throw error;
+  }
+};
+
+
+
+
+    
+
+
+
+
+export const createPayment = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${API_URL}api/loan/payment`, data,{ headers: authHeader() });
+    dispatch({
+      type: CREATE_PAYMENT,
+      payload: response.data,
+    });
+    return response.data;
+    // Aquí podrías enviar una notificación de éxito al usuario
+  } catch (error) {
+    if (error.response && error.response.status ) {
+      throw new Error("No se puedo crear el abono");
+  
+    }
+    console.error(error)
+    throw error;
+    // Aquí podrías enviar una notificación de error al usuario
+  
+  }
+};
+
+
+
+
+
+// eslint-disable-next-line func-names
+
+
+
+
+  export const updatePayment = (id, data) => async (dispatch) => {
+    try {
+      const resp = await axios.put(`${API_URL}api/loan/update-payment/${id}`, data);
+  
+      dispatch({
+        type: UPDATE_PAYMENT,
+        payload: resp.data,
+      });
+  
+      return resp.data;
+    } catch (err) {
+      return err.response;
+    }
+  };
+
+
+
+
+
+
+ 
+
+
+
+export const getAllPayment=() => async (dispatch) => {
+
+  try {
+    const response = await axios.get(`${API_URL}api/loan/get-all-payment`,{ headers: authHeader() }
+    );
+  
+    dispatch({
+      type:GET_ALL_PAYMENT,
+      payload: response.data,
+    });
+    return response.data;
+  } catch (error) {
+    dispatch(fetchLoanFailure(error.message));
+    return null
+  }
+};
+
+
+
+export const initialState = {
+  payments: [],
+  message: null,
+  error: null,
+  sendLoan: {},
+};
+
+export default function paymentReducer(state = initialState, action) {
+  switch (action.type) {
+
+
+
+    case GET_ALL_PAYMENT:
+        return {
+          ...state,
+          payments: action.payload,
+        };
+  
+
+
+    case CREATE_PAYMENT:
+      return {
+        ...state,
+        payments: action.payload,
+      };
+      
+    case CREATE_PAYMENT_SUCCESS:
+      return {
+        ...state,
+        sendLoan: action.payload,
+      };
+
+
+    case CREATE_LOAN_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+        isSuccess: false,
+        payments: null,
+      };
+
+      case SEARCH_DATE_SUCCESS:
+        return{
+      
+          ...state,
+          payments:action.payload
+      
+        }
+      
+            case UPDATE_PAYMENT:
+              return{
+              ...state,
+              payments:action.payload
+      
+      
+              }
+
+     
+
+    default:
+      return state;
+  }
+}
