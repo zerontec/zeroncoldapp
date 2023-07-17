ad19732a13ab01b59a070e88320cb34a4f4f82a8
zerontec <zerontecnologia1@gmail.com>
2023-07-17 12:05:00 -0400

    accoup module

--- src/redux/modules/accountPayables.js
+++ src/redux/modules/accountPayables.js
@@ -1,12 +1,8 @@
-
-
-
 import axios from 'axios';
 import authHeader from '../services/auth-header';
 
-const API_URL_D = "http://localhost:5040/";
-const API_URL = "https://expressjs-postgres-production-bd69.up.railway.app/"
-
+const API_URL_D = 'http://localhost:5040/';
+const API_URL = 'https://expressjs-postgres-production-bd69.up.railway.app/';
 
 const FETCH_PAYABLES_REQUEST = 'FETCH_PAYABLES_REQUEST';
 const FETCH_PAYABLES_SUCCESS = 'FETCH_PAYABLES_SUCCESS';
@@ -16,11 +12,10 @@ const CREATE_PAYABLE = 'CREATE_PAYABLE';
 const GET_PAYABLES = 'GET_PAYABLES';
 const CREATE_PAYABLE_SUCCESS = 'CREATE_PAYABLE_SUCCESS';
 const CREATE_PAYABLE_ERROR = 'CREATE_PAYABLE_ERROR';
-const  UPDATE_PAYABLE =' UPDATE_PAYABLE'
-const DELETE_PAYABLE='DELETE_PAYABLE'
-const CREATE_PAYMENT='CREATE_PAYMENT'
-const GET_ALL_PAYMENT='GET_ALL_PAYMENT'
-
+const UPDATE_PAYABLE = ' UPDATE_PAYABLE';
+const DELETE_PAYABLE = 'DELETE_PAYABLE';
+const CREATE_PAYMENT = 'CREATE_PAYMENT';
+const GET_ALL_PAYMENT = 'GET_ALL_PAYMENT';
 
 export const fetchPayablesRequest = () => ({
   type: FETCH_PAYABLES_REQUEST,
@@ -36,11 +31,10 @@ export const fetchPAyablesFailure = (error) => ({
   payload: error,
 });
 
-
 export const fetchPAYABLES = (query) => async (dispatch) => {
   dispatch(fetchPayablesRequest());
   try {
-    const response = await axios.get(`${API_URL}api/invoice/search-query?q=${query}`,{ headers: authHeader() });
+    const response = await axios.get(`${API_URL}api/invoice/search-query?q=${query}`, { headers: authHeader() });
     const data = await response.json();
     dispatch(fetchPayablesSuccess(data));
     return data;
@@ -50,26 +44,21 @@ export const fetchPAYABLES = (query) => async (dispatch) => {
   }
 };
 
-  export const getAllPayables = () => async (dispatch) => {
-    try {
-      const resp = await axios.get(`${API_URL}api/account-payable/all-account-payable`,{ headers: authHeader() });
-  
-      dispatch({ type: GET_PAYABLES, payload: resp.data });
-  
-      return resp.data;
-    } catch (err) {
-      throw err.response;
-    }
-  };
-  
+export const getAllPayables = () => async (dispatch) => {
+  try {
+    const resp = await axios.get(`${API_URL}api/account-payable/all-account-payable`, { headers: authHeader() });
 
+    dispatch({ type: GET_PAYABLES, payload: resp.data });
 
-
-
+    return resp.data;
+  } catch (err) {
+    throw err.response;
+  }
+};
 
 export const createPayable = (invoiceData) => async (dispatch) => {
   try {
-    const { data } = await axios.post(`${API_URL}api/invoice/create`, invoiceData,{ headers: authHeader() });
+    const { data } = await axios.post(`${API_URL}api/invoice/create`, invoiceData, { headers: authHeader() });
     dispatch({
       type: CREATE_PAYABLE_SUCCESS,
       payload: data,
@@ -85,100 +74,74 @@ export const createPayable = (invoiceData) => async (dispatch) => {
 };
 
 export const updatePayable = (id, data) => async (dispatch) => {
-    try {
-      const resp = await axios.put(`${API_URL}api/product/update/${id}`, data,{ headers: authHeader() });
-  
-      dispatch({
-        type: UPDATE_PAYABLE,
-        payload: resp.data,
-      });
-  
-      return resp.data;
-    } catch (err) {
-      return err.response;
+  try {
+    const resp = await axios.put(`${API_URL}api/product/update/${id}`, data, { headers: authHeader() });
+
+    dispatch({
+      type: UPDATE_PAYABLE,
+      payload: resp.data,
+    });
+
+    return resp.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const deletePayable = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`${API_URL}api/product/delete/${id}`, { headers: authHeader() });
+
+    dispatch({
+      type: DELETE_PAYABLE,
+      payload: { id },
+    });
+    return { message: 'Eliminado con exito' };
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const createAbonocxp = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${API_URL}api/account-payable/create-pay`, data, { headers: authHeader() });
+    dispatch({
+      type: CREATE_PAYMENT,
+      payload: response.data,
+    });
+    return response.data;
+    // Aquí podrías enviar una notificación de éxito al usuario
+  } catch (error) {
+    if (error.response && error.response.status) {
+      throw new Error('No se puedo crear el abono');
     }
-  };
-
-
-
-
-
-
-  export const deletePayable = (id) => async (dispatch) => {
-    try {
-      await axios.delete(`${API_URL}api/product/delete/${id}`,{ headers: authHeader() });
-  
-      dispatch({
-        type: DELETE_PAYABLE,
-        payload: { id },
-      });
-      return({message:"Eliminado con exito"})
-    } catch (err) {
-      return err.response;
+    console.error(error);
+    throw error;
+    // Aquí podrías enviar una notificación de error al usuario
+  }
+};
+export const getAllAbonoPorCuentas = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${API_URL}api/account-payable/get-all-pay`, data, { headers: authHeader() });
+    dispatch({
+      type: GET_ALL_PAYMENT,
+      payload: response.data,
+    });
+    return response.data;
+    // Aquí podrías enviar una notificación de éxito al usuario
+  } catch (error) {
+    if (error.response && error.response.status) {
+      throw new Error('No se puedo crear el abono');
     }
-  };
-
-
-  export const createAbonocxp = (data) => async (dispatch) => {
-    try {
-      const response = await axios.post(`${API_URL}api/account-payable/create-pay`, data,{ headers: authHeader() });
-      dispatch({
-        type: CREATE_PAYMENT,
-        payload: response.data,
-      });
-      return response.data;
-      // Aquí podrías enviar una notificación de éxito al usuario
-    } catch (error) {
-      if (error.response && error.response.status ) {
-        throw new Error("No se puedo crear el abono");
-    
-      }
-      console.error(error)
-      throw error;
-      // Aquí podrías enviar una notificación de error al usuario
-    
-    }
-  };
-  export const getAllAbonoPorCuentas = (data) => async (dispatch) => {
-    try {
-      const response = await axios.post(`${API_URL}api/account-payable/get-all-pay`, data,{ headers: authHeader() });
-      dispatch({
-        type: GET_ALL_PAYMENT,
-        payload: response.data,
-      });
-      return response.data;
-      // Aquí podrías enviar una notificación de éxito al usuario
-    } catch (error) {
-      if (error.response && error.response.status ) {
-        throw new Error("No se puedo crear el abono");
-    
-      }
-      console.error(error)
-      throw error;
-      // Aquí podrías enviar una notificación de error al usuario
-    
-    }
-  };
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.error(error);
+    throw error;
+    // Aquí podrías enviar una notificación de error al usuario
+  }
+};
 
 export const initialState = {
   payables: [],
-  abonos:{},
+  abonos: {},
   message: null,
   error: null,
   sendpayables: {},
@@ -192,15 +155,11 @@ export default function payablesReducer(state = initialState, action) {
         payables: action.payload,
       };
 
-      case GET_ALL_PAYMENT:
-        return{
-
+    case GET_ALL_PAYMENT:
+      return {
         ...state,
-        abonos:action.payload
-        
-
-
-      }
+        abonos: action.payload,
+      };
 
     case CREATE_PAYABLE:
       return {
@@ -208,13 +167,11 @@ export default function payablesReducer(state = initialState, action) {
         sendpayables: action.payload,
       };
 
-
-      case CREATE_PAYMENT:
-        return {
-          ...state,
-          payables: action.payload,
-        };
-
+    case CREATE_PAYMENT:
+      return {
+        ...state,
+        payables: action.payload,
+      };
 
     case CREATE_PAYABLE_ERROR:
       return {
@@ -224,9 +181,6 @@ export default function payablesReducer(state = initialState, action) {
         isSuccess: false,
         payables: null,
       };
-
-
-     
 
     default:
       return state;
